4f9abb4ca2d8e52f6c20126e1a60434b9728a93e
anike <aniketsuryawanshixz1@gmail.com>
2021-09-07 13:13:07 +0530

    new functionality

--- app/Models/Required_Docs_Creation/RestaurantDetailsRequiredDocsModel.php
+++ app/Models/Required_Docs_Creation/RestaurantDetailsRequiredDocsModel.php
@@ -14,17 +14,20 @@ class RestaurantDetailsRequiredDocsModel extends Model
     const   restaurant_details_required_docs    = 'restaurant_details_required_docs';
     const required_docs_name = 'required_docs_name';
     const required_docs_type = 'required_docs_type';
+    const required_docs_input = 'required_docs_input';
     const status = 'status';
 
     const restaurant_owner_required_docs_all = self:: restaurant_details_required_docs   . AppConfig::DOT . AppConfig::STAR;
     const restaurant_required_docs_name = self:: restaurant_details_required_docs   . AppConfig::DOT . self::required_docs_name;
     const restaurant_required_docs_type = self:: restaurant_details_required_docs   . AppConfig::DOT . self::required_docs_type;
+    const restaurant_required_docs_input = self::restaurant_details_required_docs . AppConfig::DOT . self::required_docs_input;
     const restaurant_status = self:: restaurant_details_required_docs   . AppConfig::DOT . self::status;
 
     protected $table = self:: restaurant_details_required_docs ;
     protected $fillable = [
         self::required_docs_name,
         self::required_docs_type,
+        self::required_docs_input,
         self::status
     ];
 
@@ -49,9 +52,19 @@ class RestaurantDetailsRequiredDocsModel extends Model
         return self::all();
     }
 
+
+
+
     //get only image type docs
     public static function getAllRestaurantDocsByTypeImg(){
 
         return self::select(self::required_docs_name,self::required_docs_type)->where(self::required_docs_type,"image")->get();
+    }
+
+    //get only type text docs
+    public static function getTextTypeDocs(){
+        return self::select(self::required_docs_name,self::required_docs_type,self::required_docs_input)
+                    ->where(self::required_docs_type,"!=","image")
+                    ->get();    
     }
 }
